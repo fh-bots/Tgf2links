17c5c49241b785d07413c54a9058138b60016a47
fh_bots <69167693+fh-bots@users.noreply.github.com>
2020-08-07 00:17:02 +0530

    Update __main__.py

--- tgfilestream/__main__.py
+++ tgfilestream/__main__.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import asyncio
+import asyncio , os , shutil
 import sys
 
 from aiohttp import web
